5658f1b2837d3a64182f9c148c1773675b637f54
Maarten Vandercammen <mvdcamme@vub.ac.be>
2016-04-08 14:04:58 +0200

    Updated unit tests/benchmarks

--- test/cpstak.scm
+++ test/cpstak.scm
@@ -16,4 +16,4 @@
                                      y
                                      (lambda (v3)
                                        (tak v1 v2 v3 k)))))))))))
-  (tak 20 10 5 (lambda (a) a)))
+  (tak 10 5 2 (lambda (a) a)))
