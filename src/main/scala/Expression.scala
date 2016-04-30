cf71bed9cd1b28dc877e92502aee77972de4b218
Maarten Vandercammen <mvdcamme@vub.ac.be>
2016-04-30 22:57:46 +0200

    Add zeroExp method to Expression trait
    zeroExp can be used for constructing 'dummy expressions'

--- src/main/scala/Expression.scala
+++ src/main/scala/Expression.scala
@@ -1,6 +1,12 @@
-trait Expression[A]
+trait Expression[A] {
+  def zeroExp: A
+}
 
 object Expression {
-  implicit object ANFExpExpression extends Expression[ANFExp]
-  implicit object SchemeExpExpression extends Expression[SchemeExp]
+  implicit object ANFExpExpression extends Expression[ANFExp] {
+    def zeroExp = ANFValue(ValueInteger(0))
+  }
+  implicit object SchemeExpExpression extends Expression[SchemeExp] {
+    def zeroExp = SchemeValue(ValueInteger(0))
+  }
 }
