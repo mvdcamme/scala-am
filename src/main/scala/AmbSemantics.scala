59daf28a1a5430a80fdf7b704d3d62494d3b1f53
Maarten Vandercammen <mvdcamme@vub.ac.be>
2016-05-03 15:42:51 +0200

    Added restart point for stopping a trace in the middle of a backtrack

--- src/main/scala/AmbSemantics.scala
+++ src/main/scala/AmbSemantics.scala
@@ -1,36 +1,47 @@
-//case class FailureFrame[Exp : Expression, Abs : AbstractValue, Addr : Address](frame: Frame)
-//  extends Frame {
-  //  def subsumes(that: Frame) = frame match {
-  //    case FailureFrame(thatFrame) =>
-      //      frame.subsumes(thatFrame)
-  //    case _ => false
-      //  }
-  //}
+/*
+ * Restart points
+ */
+
+case class RestartStoppedInBacktrack[Exp : Expression, Abs : AbstractValue, Addr : Address]()
+  extends RestartPoint[Exp, Abs, Addr]
+
+/*
+ * Frames
+ */
+
 case class UndoActionFrame[Exp : Expression, Abs : AbstractValue, Addr : Address](action: ActionSingleTraced[Exp, Abs, Addr])
   extends Frame {
     def subsumes(that: Frame) = that.equals(this)
   }
 
+/*
+ * Normal actions
+ */
+
 case class ActionPopFailKontTraced[Exp : Expression, Abs : AbstractValue, Addr : Address]()
   extends Action[Exp, Abs, Addr]
 case class ActionPushFailKontTraced[Exp : Expression, Abs : AbstractValue, Addr : Address]
-  (failureFrame: Frame) extends Action[Exp, Abs, Addr]
-
+  (failureFrame: Frame)
+  extends Action[Exp, Abs, Addr]
 
 /*
  * Single actions
  */
 
-abstract class ActionSingleTraced[Exp : Expression, Abs : AbstractValue, Addr : Address] extends Action[Exp, Abs, Addr]
+abstract class ActionSingleTraced[Exp : Expression, Abs : AbstractValue, Addr : Address]
+  extends Action[Exp, Abs, Addr]
 
 case class ActionSinglePopKontTraced[Exp : Expression, Abs : AbstractValue, Addr : Address]()
   extends ActionSingleTraced[Exp, Abs, Addr]
 case class ActionSinglePushKontTraced[Exp : Expression, Abs : AbstractValue, Addr : Address]
-  (frame: Frame) extends ActionSingleTraced[Exp, Abs, Addr]
+  (frame: Frame)
+  extends ActionSingleTraced[Exp, Abs, Addr]
 case class ActionSingleSaveSpecificEnvTraced[Exp : Expression, Abs : AbstractValue, Addr : Address]
-  (ρ: Environment[Addr]) extends ActionSingleTraced[Exp, Abs, Addr]
+  (ρ: Environment[Addr])
+  extends ActionSingleTraced[Exp, Abs, Addr]
 case class ActionSingleSaveValTraced[Exp : Expression, Abs : AbstractValue, Addr : Address]
-  (value: Abs) extends ActionSingleTraced[Exp, Abs, Addr]
+  (value: Abs)
+  extends ActionSingleTraced[Exp, Abs, Addr]
 case class ActionSingleRestoreEnvTraced[Exp : Expression, Abs : AbstractValue, Addr : Address]()
   extends ActionSingleTraced[Exp, Abs, Addr]
 case class ActionSingleRestoreValTraced[Exp : Expression, Abs : AbstractValue, Addr : Address]()
