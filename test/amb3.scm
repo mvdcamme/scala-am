ebbf7bae9673dcf4cc2a67b3648570fd2fe0b048
Maarten Vandercammen <mvdcamme@vub.ac.be>
2016-05-06 13:59:46 +0200

    Add new testcase

---
+++ test/amb3.scm
@@ -0,0 +1,19 @@
+(define (require pred)
+  (if (not pred)
+      (amb)
+      'stop))
+
+(define (an-integer-between loww highh)
+  (if (> loww highh)
+      (amb)
+      (amb loww (an-integer-between (+ loww 1) highh))))
+
+(define (a-pythagorean-triple-between low high)
+  (let* ((i (an-integer-between low high))
+         (j (an-integer-between (+ i 1) high))
+         (k (an-integer-between (+ j 1) high)))
+    (require (= (+ (* i i) (* j j)) (* k k)))
+    (display (+ (* i 10000) (* j 100) k))
+    (amb)))
+
+(a-pythagorean-triple-between 1 100)
