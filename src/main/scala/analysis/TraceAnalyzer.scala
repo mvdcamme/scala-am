3869dfae78a7fb1f2bfe3ce9975126c1749d5399
Maarten Vandercammen <mvdcamme@vub.ac.be>
2016-09-11 20:08:57 +0200

    Fix bug in finding variables that are bound inside the trace

--- src/main/scala/analysis/TraceAnalyzer.scala
+++ src/main/scala/analysis/TraceAnalyzer.scala
@@ -6,12 +6,8 @@ object TraceAnalyzer {
     trace.foldRight(Set[HybridAddress.A]())((stateInfo, boundAddresses) => stateInfo match {
       case (_, infos) =>
         infos.find[Set[HybridAddress.A]](
-          { case AddressesAllocated(_) => true; case _ => false},
-          { case AddressesAllocated(addresses) => boundAddresses ++ addresses.toSet }).getOrElse(boundAddresses)
-      case (_, infos) =>
-        infos.find[Set[HybridAddress.A]](
-          { case AddressesReassigned(_) => true; case _ => false},
-          { case AddressesReassigned(addresses) => boundAddresses ++ addresses.toSet }).getOrElse(boundAddresses)
+          { case AddressesAllocated(_) => true; case AddressesReassigned(_) => true; case _ => false},
+          { case AddressesAllocated(addresses) => boundAddresses ++ addresses.toSet; case AddressesReassigned(addresses) => boundAddresses ++ addresses.toSet }).getOrElse(boundAddresses)
       case _ => boundAddresses
     })
 
