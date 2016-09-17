14b8e34b79dbfdb3db751f2d7dbd4bed73c0c0f0
Maarten Vandercammen <mvdcamme@vub.ac.be>
2016-09-17 21:27:51 +0200

    Add SignalStartAnalysis for signalling the HybridMachine to start a run-time analysis

--- src/main/scala/InterpreterStep.scala
+++ src/main/scala/InterpreterStep.scala
@@ -6,9 +6,18 @@ trait TracingSignal[Exp, Abs, Addr]
 case class SignalFalse[Exp : Expression, Abs : JoinLattice, Addr : Address]()
   extends TracingSignal[Exp, Abs, Addr]
 case class SignalStartLoop[Exp : Expression, Abs : JoinLattice, Addr : Address]
-  (label: List[Exp]) extends TracingSignal[Exp, Abs, Addr]
+  (label: List[Exp])
+  extends TracingSignal[Exp, Abs, Addr]
 case class SignalEndLoop[Exp : Expression, Abs : JoinLattice, Addr : Address]
-  (label: List[Exp], restartPoint: RestartPoint[Exp, Abs, Addr]) extends TracingSignal[Exp, Abs, Addr]
+  (label: List[Exp], restartPoint: RestartPoint[Exp, Abs, Addr])
+  extends TracingSignal[Exp, Abs, Addr]
+
+/**
+  * Signals the tracer to start a run-time static analysis.
+  * Has no effect on tracing.
+  */
+case class SignalStartAnalysis[Exp : Expression, Abs : JoinLattice, Addr : Address]()
+  extends TracingSignal[Exp, Abs, Addr]
 
 /*
  * Interpreter return
