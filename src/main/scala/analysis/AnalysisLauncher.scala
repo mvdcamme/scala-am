a8b2ff4dc5e7ba12117d20c8c0c26c316fc3f7d3
Maarten Vandercammen <mvdcamme@vub.ac.be>
2016-10-01 22:36:51 +0200

    Dump (WIP)

--- src/main/scala/analysis/AnalysisLauncher.scala
+++ src/main/scala/analysis/AnalysisLauncher.scala
@@ -1,48 +1,50 @@
-//abstract class AnalysisLauncher[Abs : IsSchemeLattice, Exp : Expression] {
-//
-//  /* The concrete program state the static analysis gets as input. This state is then converted to an
-//   * abstract state and fed to the AAM. */
-//  type PS = HybridMachine[Abs, Exp]#PS
-//  /* The specific type of AAM used for this analysis: an AAM using the HybridLattice, HybridAddress and ZeroCFA
-//   * components. */
-//  type SpecFree = Free[Exp, Abs, HybridAddress.A, HybridTimestamp.T]
-//  /* The specific environment used in the concrete state: an environment using the HybridAddress components. */
-//  type SpecEnv = Environment[HybridAddress.A]
-//
-//  protected def switchToAbstract(): Unit = {
-//    Logger.log("HybridMachine switching to abstract", Logger.I)
-//    HybridTimestamp.switchToAbstract()
-//  }
-//
-//  protected def switchToConcrete(): Unit = {
-//    Logger.log("HybridMachine switching to concrete", Logger.I)
-//    HybridTimestamp.switchToConcrete()
-//  }
-//
-//  protected def wrapRunAnalysis(runAnalysis: () => StaticAnalysisResult): StaticAnalysisResult = {
-//    Logger.log("analyzing", Logger.I)
-//    switchToAbstract()
-//    val result = runAnalysis()
-//    switchToConcrete()
-//    result
-//  }
-//
-//  /**
-//   * Converts the given state to a new state corresponding to the state employed by the given P4F machine.
-//   * @param free The P4F machine for which a new, converted, state must be generated.
-//   * @param sem The semantics to be used during the analysis.
-//   * @param programState The program state to be converted.
-//   */
-//  protected def convertState(free: Free[Exp, Abs, HybridAddress.A, HybridTimestamp.T],
-//                             sem: SemanticsTraced[Exp, ConcreteConcreteLattice.L, HybridAddress.A, HybridTimestamp.T],
-//                             programState: PS): free.States = {
-//    val (control, _, store, kstore, a, t) = programState.convertState[Abs](free, sem)
-//    val convertedControl = control match {
-//      case ConvertedControlError(reason) => free.ControlError(reason)
-//      case ConvertedControlEval(exp, env) => free.ControlEval(exp, env)
-//      case ConvertedControlKont(v) => free.ControlKont(v)
-//    }
-//    free.States(Set(free.Configuration(convertedControl, a, t)), store, kstore)
-//  }
-//
-//}
+abstract class AnalysisLauncher[Abs : IsConvertableLattice, Exp : Expression] {
+
+  /* The concrete program state the static analysis gets as input. This state is then converted to an
+   * abstract state and fed to the AAM. */
+  type PS = HybridMachine[Exp]#PS
+  /* The specific type of AAM used for this analysis: an AAM using the HybridLattice, HybridAddress and ZeroCFA
+   * components. */
+  type SpecFree = Free[Exp, Abs, HybridAddress.A, HybridTimestamp.T]
+  /* The specific environment used in the concrete state: an environment using the HybridAddress components. */
+  type SpecEnv = Environment[HybridAddress.A]
+
+  protected def switchToAbstract(): Unit = {
+    Logger.log("HybridMachine switching to abstract", Logger.I)
+    HybridTimestamp.switchToAbstract()
+  }
+
+  protected def switchToConcrete(): Unit = {
+    Logger.log("HybridMachine switching to concrete", Logger.I)
+    HybridTimestamp.switchToConcrete()
+  }
+
+  protected def wrapRunAnalysis(runAnalysis: () => StaticAnalysisResult): StaticAnalysisResult = {
+    Logger.log("analyzing", Logger.I)
+    switchToAbstract()
+    val result = runAnalysis()
+    switchToConcrete()
+    result
+  }
+
+  /**
+   * Converts the given state to a new state corresponding to the state employed by the given P4F machine.
+   * @param free The P4F machine for which a new, converted, state must be generated.
+   * @param concSem The semantics currently being used.
+   * @param abstSem The semantics to be used during the analysis.
+   * @param programState The program state to be converted.
+   */
+  protected def convertState(free: Free[Exp, Abs, HybridAddress.A, HybridTimestamp.T],
+                             concSem: SemanticsTraced[Exp, ConcreteConcreteLattice.L, HybridAddress.A, HybridTimestamp.T],
+                             abstSem: BaseSchemeSemantics[Abs, HybridAddress.A, HybridTimestamp.T],
+                             programState: PS): free.States = {
+    val (control, _, store, kstore, a, t) = programState.convertState[Abs](free, concSem, abstSem)
+    val convertedControl = control match {
+      case ConvertedControlError(reason) => free.ControlError(reason)
+      case ConvertedControlEval(exp, env) => free.ControlEval(exp, env)
+      case ConvertedControlKont(v) => free.ControlKont(v)
+    }
+    free.States(Set(free.Configuration(convertedControl, a, t)), store, kstore)
+  }
+
+}
--- src/main/scala/analysis/PointsToAnalysis.scala
+++ src/main/scala/analysis/PointsToAnalysis.scala
@@ -1,49 +1,51 @@
-//class PointsToAnalysis[Exp: Expression, L : JoinLattice, Addr : Address, Time : Timestamp] {
-//
-//  private def joinStores(free: Free[Exp, L, Addr, Time])
-//                        (stores: Set[Store[Addr, L]]): Set[(Addr, L)] = {
-//    val joinedStore = stores.foldLeft(Store.initial(Set()): Store[Addr, L])
-//    { case (joinedStore, store) => joinedStore.join(store) }
-//    joinedStore.toSet
-//  }
-//
-//  private def analyzeOutput(free: Free[Exp, L, Addr, Time], pointsTo: L => Int)
-//                           (output: free.FreeOutput): Set[(Addr, Int)] = {
-//    val storeValues = joinStores(free)(output.finalStores)
-//    val initial = Set[(Addr, Int)]()
-//    val result: Set[(Addr, Int)] = storeValues.foldLeft(initial)({ case (result, (address, value)) =>
-//      val numberOfObjectsPointedTo: Int = pointsTo(value)
-//      if (numberOfObjectsPointedTo > 1) {
-//        /* List all addresses pointing to more than one value */
-//        result + ((address, numberOfObjectsPointedTo))
-//      } else {
-//        result
-//      } })
-//    Logger.log(s"Static points-to analysis completed, resulting set equals $result", Logger.D)
-//    result
-//  }
-//
-//  def analyze(free: Free[Exp, L, Addr, Time], sem: Semantics[Exp, L, Addr, Time], pointsTo: L => Int)
-//             (startState: free.States, isInitial: Boolean): Set[(Addr, Int)] = {
-//    Logger.log(s"Starting static points-to analysis", Logger.I)
-//    val output = free.kickstartEval(startState, sem, None, None)
-//    analyzeOutput(free, pointsTo)(output)
-//  }
-//}
-//
-//class PointsToAnalysisLauncher[Abs : IsSchemeLattice, Exp : Expression]
-//  (concSem: SemanticsTraced[Exp, ConcreteConcreteLattice.L, HybridAddress.A, HybridTimestamp.T],
-//   abstSem: Semantics[Exp, Abs, HybridAddress.A, HybridTimestamp.T]) extends AnalysisLauncher[Abs, Exp] {
-//
-//  val pointsToAnalysis = new PointsToAnalysis[Exp, Abs, HybridAddress.A, HybridTimestamp.T]
-//
-//  def runStaticAnalysis(currentProgramState: PS): StaticAnalysisResult =
-//    wrapRunAnalysis(() => {
-//      val free: SpecFree = new SpecFree()
-//      val startStates = convertState(free, concSem, currentProgramState)
-//      val result = pointsToAnalysis.analyze(free, abstSem, hybridLattice.pointsTo)(startStates, false)
-//      Logger.log(s"Static points-to analysis result is $result", Logger.U)
-//      PointsToSet(result)
-//    })
-//
-//}
+class PointsToAnalysis[Exp: Expression, L : JoinLattice, Addr : Address, Time : Timestamp] {
+
+  private def joinStores(free: Free[Exp, L, Addr, Time])
+                        (stores: Set[Store[Addr, L]]): Set[(Addr, L)] = {
+    val joinedStore = stores.foldLeft(Store.initial(Set()): Store[Addr, L])
+    { case (joinedStore, store) => joinedStore.join(store) }
+    joinedStore.toSet
+  }
+
+  private def analyzeOutput(free: Free[Exp, L, Addr, Time], pointsTo: L => Int)
+                           (output: free.FreeOutput): Set[(Addr, Int)] = {
+    val storeValues = joinStores(free)(output.finalStores)
+    val initial = Set[(Addr, Int)]()
+    val result: Set[(Addr, Int)] = storeValues.foldLeft(initial)({ case (result, (address, value)) =>
+      val numberOfObjectsPointedTo: Int = pointsTo(value)
+      if (numberOfObjectsPointedTo > 1) {
+        /* List all addresses pointing to more than one value */
+        result + ((address, numberOfObjectsPointedTo))
+      } else {
+        result
+      } })
+    Logger.log(s"Static points-to analysis completed, resulting set equals $result", Logger.D)
+    result
+  }
+
+  def analyze(free: Free[Exp, L, Addr, Time], sem: Semantics[Exp, L, Addr, Time], pointsTo: L => Int)
+             (startState: free.States, isInitial: Boolean): Set[(Addr, Int)] = {
+    Logger.log(s"Starting static points-to analysis", Logger.I)
+    val output = free.kickstartEval(startState, sem, None, None)
+    analyzeOutput(free, pointsTo)(output)
+  }
+}
+
+class PointsToAnalysisLauncher[Abs : IsConvertableLattice]
+  (concSem: SemanticsTraced[SchemeExp, ConcreteConcreteLattice.L, HybridAddress.A, HybridTimestamp.T],
+   abstSem: BaseSchemeSemantics[Abs, HybridAddress.A, HybridTimestamp.T]) extends AnalysisLauncher[Abs, SchemeExp] {
+
+  val abs = implicitly[IsConvertableLattice[Abs]]
+
+  val pointsToAnalysis = new PointsToAnalysis[SchemeExp, Abs, HybridAddress.A, HybridTimestamp.T]
+
+  def runStaticAnalysis(currentProgramState: PS): StaticAnalysisResult =
+    wrapRunAnalysis(() => {
+      val free: SpecFree = new SpecFree()
+      val startStates = convertState(free, concSem, abstSem, currentProgramState)
+      val result = pointsToAnalysis.analyze(free, abstSem, abs.latticeInfoProvider.pointsTo)(startStates, false)
+      Logger.log(s"Static points-to analysis result is $result", Logger.U)
+      PointsToSet(result)
+    })
+
+}
