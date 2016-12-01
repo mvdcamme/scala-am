8429affd4891e1f6c682af9baca4f4d09e3f7c1c
Maarten Vandercammen <mvdcamme@vub.ac.be>
2016-12-01 13:50:28 +0100

    Experiment with other, non-tracing HybridMachine

---
+++ src/main/scala/machine/HybridConcreteMachine.scala
@@ -0,0 +1,183 @@
+//class HybridConcreteMachine[
+//    Exp: Expression, Abs: JoinLattice, Addr: Address, Time: Timestamp]
+//    extends EvalKontMachine[Exp, Abs, Addr, Time] {
+//  def name = "ConcreteMachine"
+//
+//  trait ConcreteMachineOutput extends Output[Abs] {
+//    def toDotFile(path: String) =
+//      println("Not generating graph for ConcreteMachine")
+//  }
+//
+//  case class ConcreteMachineOutputError(time: Double,
+//                                        numberOfStates: Int,
+//                                        err: String)
+//      extends ConcreteMachineOutput {
+//    def finalValues = {
+//      println(s"Execution failed: $err")
+//      Set()
+//    }
+//    def containsFinalValue(v: Abs) = false
+//    def timedOut = false
+//  }
+//  case class ConcreteMachineOutputTimeout(time: Double, numberOfStates: Int)
+//      extends ConcreteMachineOutput {
+//    def finalValues = Set()
+//    def containsFinalValue(v: Abs) = false
+//    def timedOut = true
+//  }
+//  case class ConcreteMachineOutputValue(time: Double,
+//                                        numberOfStates: Int,
+//                                        v: Abs)
+//      extends ConcreteMachineOutput {
+//    def finalValues = Set(v)
+//    def containsFinalValue(v2: Abs) = v == v2
+//    def timedOut = false
+//  }
+//
+//  case class State(control: Control, store: Store[Addr, Abs], stack: List[Frame], t: Time) extends
+//  ConcreteTracingProgramState[Exp, Addr, Time] {
+//
+//    implicit val abs = implicitly[Abs]
+//    implicit val addr = implicitly[Addr]
+//    implicit val time = implicitly[Time]
+//
+//    def halted = control match {
+//      case ControlError(_) => true
+//      case ControlKont(_) => stack.isEmpty
+//      case _ => false
+//    }
+//  }
+//
+//  /**
+//    * Performs the evaluation of an expression, possibly writing the output graph
+//    * in a file, and returns the set of final states reached
+//    */
+//  def eval(exp: Exp,
+//           sem: Semantics[Exp, Abs, Addr, Time],
+//           graph: Boolean,
+//           timeout: Option[Long]): Output[Abs] = {
+//    def loop(control: Control,
+//             store: Store[Addr, Abs],
+//             stack: List[Frame],
+//             t: Time,
+//             start: Long,
+//             count: Int): ConcreteMachineOutput = {
+//      if (timeout.map(System.nanoTime - start > _).getOrElse(false)) {
+//        ConcreteMachineOutputTimeout(
+//          (System.nanoTime - start) / Math.pow(10, 9),
+//          count)
+//      } else {
+//        control match {
+//          case ControlEval(e, env) =>
+//            val edges = sem.stepEval(e, env, store, t)
+//            if (edges.size == 1) {
+//              edges.head._1 match {
+//                case ActionReachedValue(v, store2, _) =>
+//                  loop(ControlKont(v),
+//                       store2,
+//                       stack,
+//                       time.tick(t),
+//                       start,
+//                       count + 1)
+//                case ActionPush(frame, e, env, store2, _) =>
+//                  loop(ControlEval(e, env),
+//                       store2,
+//                       frame :: stack,
+//                       time.tick(t),
+//                       start,
+//                       count + 1)
+//                case ActionEval(e, env, store2, _) =>
+//                  loop(ControlEval(e, env),
+//                       store2,
+//                       stack,
+//                       time.tick(t),
+//                       start,
+//                       count + 1)
+//                case ActionStepIn(fexp, _, e, env, store2, _, _) =>
+//                  loop(ControlEval(e, env),
+//                       store2,
+//                       stack,
+//                       time.tick(t, fexp),
+//                       start,
+//                       count + 1)
+//                case ActionError(err) =>
+//                  ConcreteMachineOutputError(
+//                    (System.nanoTime - start) / Math.pow(10, 9),
+//                    count,
+//                    err.toString)
+//              }
+//            } else {
+//              ConcreteMachineOutputError(
+//                (System.nanoTime - start) / Math.pow(10, 9),
+//                count,
+//                s"execution was not concrete (got ${edges.size} actions instead of 1)")
+//            }
+//          case ControlKont(v) =>
+//            /* pop a continuation */
+//            stack match {
+//              case frame :: tl =>
+//                val edges = sem.stepKont(v, frame, store, t)
+//                if (edges.size == 1) {
+//                  edges.head._1 match {
+//                    case ActionReachedValue(v, store2, _) =>
+//                      loop(ControlKont(v),
+//                           store2,
+//                           tl,
+//                           time.tick(t),
+//                           start,
+//                           count + 1)
+//                    case ActionPush(frame, e, env, store2, _) =>
+//                      loop(ControlEval(e, env),
+//                           store2,
+//                           frame :: tl,
+//                           time.tick(t),
+//                           start,
+//                           count + 1)
+//                    case ActionEval(e, env, store2, _) =>
+//                      loop(ControlEval(e, env),
+//                           store2,
+//                           tl,
+//                           time.tick(t),
+//                           start,
+//                           count + 1)
+//                    case ActionStepIn(fexp, _, e, env, store2, _, _) =>
+//                      loop(ControlEval(e, env),
+//                           store2,
+//                           tl,
+//                           time.tick(t, fexp),
+//                           start,
+//                           count + 1)
+//                    case ActionError(err) =>
+//                      ConcreteMachineOutputError(
+//                        (System.nanoTime - start) / Math.pow(10, 9),
+//                        count,
+//                        err.toString)
+//                  }
+//                } else {
+//                  ConcreteMachineOutputError(
+//                    (System.nanoTime - start) / Math.pow(10, 9),
+//                    count,
+//                    s"execution was not concrete (got ${edges.size} actions instead of 1)")
+//                }
+//              case Nil =>
+//                ConcreteMachineOutputValue(
+//                  (System.nanoTime - start) / Math.pow(10, 9),
+//                  count,
+//                  v)
+//            }
+//          case ControlError(err) =>
+//            ConcreteMachineOutputError(
+//              (System.nanoTime - start) / Math.pow(10, 9),
+//              count,
+//              err.toString)
+//        }
+//      }
+//    }
+//    loop(ControlEval(exp, Environment.initial[Addr](sem.initialEnv)),
+//         Store.initial[Addr, Abs](sem.initialStore),
+//         Nil,
+//         time.initial(""),
+//         System.nanoTime,
+//         0)
+//  }
+//}
