213d84e6aafc49a4bda698894966861415c0b60e
Maarten Vandercammen <mvdcamme@vub.ac.be>
2017-01-09 10:46:25 +0100

    Convert certain characters that are not (correctly) displayed in the webpage by their corresponding HTML entities

--- src/main/scala/Descriptor.scala
+++ src/main/scala/Descriptor.scala
@@ -15,17 +15,30 @@ trait Descriptor[+Component] {
     */
   def describeCollapsableList[A](elements: Iterable[A],
                                  name: String,
-                                 describe: A => String = (x: A) => x.toString,
+                                 describe: A => String = (x: A) => convertHTML(x.toString),
                                  divClass: Option[String] = None):
   String =
     putIntoCollapsableList(elements.map(describe), name, divClass)
 
   def putIntoCollapsableList(iterable: Iterable[String], name: String, divClass: Option[String] = None): String =
-    divClass.fold("")((name) => "<div class=\"" + name + "\">") + s"<UL class=" + "\"" + collapsableClass + "\">\n" +
-    s"\t<LI><SPAN>$name</SPAN>\n\t\t<UL>\n" + iterable.map( (el) => s"\t\t\t<LI><SPAN>$el</SPAN></LI>" ).mkString("\n") +
+    divClass.fold("")((name) => "<div class=\"" + convertHTML(name) + "\">") + s"<UL class=" +
+    "\"" + collapsableClass + "\">\n" + s"\t<LI><SPAN>${convertHTML(name)}</SPAN>\n\t\t<UL>\n" +
+    iterable.map( (el) => s"\t\t\t<LI><SPAN>$el</SPAN></LI>" ).mkString("\n") +
     "\t\t</UL>\n\t</LI></UL>" + divClass.fold("")((_) => "</div>")
+
+  /**
+    * Converts certain character sequences in the given HTML string that will not be accurately displayed on the webpage
+    * (such as the '<' or '>' characters) by their proper HTML entities.
+    * @param html The string in which to replace the characters.
+    * @return A new string representing the HTML in which all necessary characters have been replaced by their proper
+    *         entities.
+    */
+  def convertHTML(html: String): String = {
+    val stringsToReplace = List(("<", "&#60;"), (">", "&#62;"))
+    stringsToReplace.foldLeft(html)( (converted, toReplace) => converted.replace(toReplace._1, toReplace._2))
+  }
 }
 
 class BasicDescriptor[+Component] extends Descriptor[Component] {
-  def describe[U >: Component](component: U): String = component.toString
+  def describe[U >: Component](component: U): String = convertHTML(component.toString)
 }
