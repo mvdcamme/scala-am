8d25bace052f99a640b3f8b54a13a1d10f0e098e
Maarten Vandercammen <mvdcamme@vub.ac.be>
2017-02-06 19:34:26 +0100

    Change implementation of fact.scm benchmark to maximise effect of garbage collection before the abstraction of the state

--- test/fact.scm
+++ test/fact.scm
@@ -1,6 +1,8 @@
 ; expected result: 40320
-(letrec ((fact (lambda (n)
-                 (if (= n 0)
-                   1
-                   (* n (fact (- n 1)))))))
+(letrec ((fact (lambda (x)
+                 (define (loop n acc)
+                   (if (= n 0)
+                       acc
+                       (loop (- n 1) (* n acc))))
+                 (loop x 1))))
   (fact 8))
