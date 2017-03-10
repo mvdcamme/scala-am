ac976aa874c5c27c90f4a9308167e2101abf57fa
Maarten Vandercammen <mvdcamme@vub.ac.be>
2017-03-10 16:33:03 +0100

    Refactoring: move code for pruning edges during propagation phase into separate class

--- src/main/scala/analysis/incremental/PropagateRunTimeInfo.scala
+++ src/main/scala/analysis/incremental/PropagateRunTimeInfo.scala
@@ -13,98 +13,11 @@ class PropagateRunTimeInfo[Exp: Expression,
   import usesGraph._
 
   val filterEdgeFilterAnnotations = new FilterEdgeFilterAnnotations[Exp, Abs, Addr, Time, State]
+  val filterPropagationEdges = new FilterPropagationEdges[Exp, Abs, Addr, Time, State]
 
   val LogPropagation = Logger.D
 
   type ActionEdge = List[ActionReplay[Exp, Abs, Addr]]
-  type FilterEdge = List[FilterAnnotation]
-
-  private def filterWithStore(newState: State, edges: Set[Edge]): Set[Edge] = {
-
-    def hasSemanticsFilter(edge: (EdgeAnnotation2, State), filter: SemanticsFilterAnnotation): Boolean =
-      edge._1.filters.contains(filter)
-    /*
-     * If there is a ThenBranchTaken-annotation and current state did NOT evaluate to true, take all edges not
-     * containing a ThenBranchTaken-annotation.
-     */
-    val filteredTrue: Set[(EdgeAnnotation2, State)] = if (edges.exists(hasSemanticsFilter(_, ThenBranchTaken)) &&
-      (! actionRApplier.evaluatedTrue(newState))) {
-      edges.filter(!hasSemanticsFilter(_, ThenBranchTaken))
-    } else {
-      edges
-    }
-    /*
-     * If there is an ElseBranchTaken-annotation and current state did NOT evaluate to false, take all edges not
-     * containing an ElseBranchTaken-annotation.
-     */
-    val filteredFalse: Set[(EdgeAnnotation2, State)] = if (edges.exists(hasSemanticsFilter(_, ElseBranchTaken)) &&
-      (!actionRApplier.evaluatedFalse(newState))) {
-      edges.filter(! hasSemanticsFilter(_, ElseBranchTaken))
-    } else {
-      edges
-    }
-
-    val filteredEdges = filteredTrue.intersect(filteredFalse)
-    if (filteredEdges.size != edges.size) {
-      Logger.log(s"## Difference between edges and filteredEdges! ##", Logger.U)
-    }
-    filteredEdges
-  }
-
-  private def filterWithKStore(newState: State, edges: Set[Edge]): Set[Edge] = {
-
-    type RelevantFrame = FrameFuncallOperands[Abs, HybridAddress.A, HybridTimestamp.T]
-    val sabs = implicitly[IsSchemeLattice[Abs]]
-    /*
-     * Checks if the given frame directly leads to a closure call. If yes, returns the frame, casted as a RelevantFrame.
-     * If not, returns None.
-     */
-    def frameLeadsToClosureCall(frame: Frame): Option[RelevantFrame] = frame match {
-      case frame: RelevantFrame =>
-        if (sabs.getClosures(frame.f).nonEmpty && frame.toeval.isEmpty) {
-          Some(frame)
-        } else {
-          None
-        }
-      case _ =>
-        None
-    }
-
-    val actualKonts = actionRApplier.getKonts(newState)
-    val relevantActualFrames: Set[RelevantFrame] = actualKonts.map(_.frame).flatMap((frame: Frame) => {
-      val optionRelevantFrame = frameLeadsToClosureCall(frame)
-      optionRelevantFrame.fold[Set[RelevantFrame]](Set())((relevantFrame: RelevantFrame) => Set(relevantFrame))
-    })
-
-    /*
-     * edgesWith: all edges that contain a FrameFollowed EdgeAnnotation with a frame that leads to a closure call.
-     * edgesWithout: all edges that don't satisfy the above condition.
-     */
-    val (edgesWith, edgesWithout) = edges.partition( (edge) => {
-      val filters = edge._1.filters
-      filters.machineExists({
-        case annot: FrameFollowed[Abs] =>
-          frameLeadsToClosureCall(annot.frame).isDefined
-        case _ =>
-          false
-      })
-    })
-
-    val filteredEdgesWith: Set[Edge] = relevantActualFrames.flatMap( (relevantFrame) => {
-      val actualClosures = sabs.getClosures(relevantFrame.f)
-      val actualLambdas: Set[Exp] = actualClosures.map(_._1)
-      val result: Set[Edge] = edgesWith.filter( (edge: Edge) => {
-        edge._1.filters.machineExists({
-          case actionClosureCall: ClosureCallMark[Exp, Abs] =>
-            actualLambdas.contains(actionClosureCall.lambda)
-          case _ =>
-            false
-        })
-      })
-      result
-    })
-    filteredEdgesWith ++ edgesWithout
-  }
 
   /*
    * originalState: The original state that was present in the initial abstract graph.
@@ -168,8 +81,8 @@ class PropagateRunTimeInfo[Exp: Expression,
      */
     val edges: Set[Edge] = originalStates.flatMap(prunedGraph.edges.getOrElse(_, Set()))
     val afterSubsumptionEdges: Set[Edge] = afterSubsumptionStates.flatMap(prunedGraph.edges.getOrElse(_, Set()))
-    val storeFilteredEdges = filterWithStore(newState, afterSubsumptionEdges)
-    val kstoreFilteredEdges = filterWithKStore(newState, storeFilteredEdges)
+    val storeFilteredEdges = filterPropagationEdges.filterWithStore(newState, afterSubsumptionEdges)
+    val kstoreFilteredEdges = filterPropagationEdges.filterWithKStore(newState, storeFilteredEdges)
 
     val (subsumptionEdges, nonSubsumptionEdges) = kstoreFilteredEdges.partition(isSubsumptionEdge)
 
