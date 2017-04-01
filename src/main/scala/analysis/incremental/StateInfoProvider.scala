a1b6e83fdb57cdfff1d9aa5877b45e0452c4dca7
Maarten Vandercammen <mvdcamme@vub.ac.be>
2017-04-01 23:09:55 +0200

    Revert "Add extra opportunity for optimising run-time info propagation when one continuation address in the original state is split over two continuation addresses in the "new" state"
    This reverts commit 8c36def69b1487a9fd9bb030c1459f33119f9e93.

--- src/main/scala/analysis/incremental/StateInfoProvider.scala
+++ src/main/scala/analysis/incremental/StateInfoProvider.scala
@@ -1,6 +1,6 @@
 trait StateInfoProvider[State] {
 
   def deltaStoreEmpty(state1: State, state2: State): Boolean
-  def deltaKStore(state1: State, state2: State): Option[Either[Iterable[(KontAddr, KontAddr)], KontStore[KontAddr]]]
+  def deltaKStore(state1: State, state2: State): Option[Iterable[(KontAddr, KontAddr)]]
 
 }
