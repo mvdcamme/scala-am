870f1554921554cd41884cb91263c8e35fef1f0e
Maarten Vandercammen <mvdcamme@vub.ac.be>
2017-08-24 10:10:06 +0200

    Add SymbolicNodes

--- src/main/scala/analysis/concolic/SymbolicNode.scala
+++ src/main/scala/analysis/concolic/SymbolicNode.scala
@@ -1,3 +1,7 @@
-case class SymbolicNode(stringRepresentation: String,
-                        left: Option[SymbolicNode],
-                        right: Option[SymbolicNode])
+trait SymbolicNode
+
+case class BranchSymbolicNode(branch: BranchConstraint,
+                              thenBranchTaken: Boolean,
+                              thenBranch: Option[SymbolicNode],
+                              elseBranch: Option[SymbolicNode])
+case class StatementSymbolicNode(statement: StatementConstraint, followUp: Option[SymbolicNode])
