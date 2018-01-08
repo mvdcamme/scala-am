b10a90d976cda1fcd850f74ef77f0ac5e1f3c405
Maarten Vandercammen <mvdcamme@vub.ac.be>
2018-01-08 17:38:58 +0100

    Fix bug in optimisation of IntMinus ArithmeticalConcolicExpressions
    Optimisation was incorrect because it did not take into account the order of arguments: e.g., '1' in (- x 1) could be moved to the first position, with 'x' being moved to the second
    TODO Make the new optimisation generic in the number of arguments the minus takes

--- src/main/scala/analysis/incremental/ConstraintOptimizer.scala
+++ src/main/scala/analysis/incremental/ConstraintOptimizer.scala
@@ -4,6 +4,8 @@ import backend.tree._
 
 object ConstraintOptimizer {
 
+  type EitherReduced = Either[ConcolicExpression, Int]
+
   def splitReduced(exps: List[ConcolicExpression]): (List[Int], List[ConcolicExpression]) = {
     exps.foldLeft[(List[Int], List[ConcolicExpression])]((Nil, Nil))((acc, exp) => reduceToInt(exp) match {
       case Left(exp) => (acc._1, acc._2 :+ exp)
@@ -11,10 +13,16 @@ object ConstraintOptimizer {
     })
   }
 
-  private def reduceArithExp(exp: ArithmeticalConcolicExpression, reduce: List[Int] => Int): Either[ArithmeticalConcolicExpression, Int] = {
+  /**
+    * Attempts to reduce an ArithmeticConcolicExpression that uses a commutative operator.
+    * @param exp
+    * @param reduce
+    * @return
+    */
+  private def reduceCommutativeArithExp(exp: ArithmeticalConcolicExpression, reduce: List[Int] => Int): EitherReduced = {
     val (ints, args) = splitReduced(exp.exps)
     val reducedInts = reduce(ints)
-    val result: Either[ArithmeticalConcolicExpression, Int] = if (args.isEmpty) {
+    val result: EitherReduced = if (args.isEmpty) {
       /* All arguments could be reduced to an integer */
       Right(reducedInts)
     } else if (ints.nonEmpty) {
@@ -27,22 +35,82 @@ object ConstraintOptimizer {
     result
   }
 
-  private def optimizeArithExp(exp: ArithmeticalConcolicExpression): Either[ArithmeticalConcolicExpression, Int] = exp.op match {
-    case IntPlus => reduceArithExp(exp, _.sum)
-    case IntMinus =>
-      def subtract(list: List[Int]): Int = list match {
-        case Nil => 0
-        case head :: rest =>
-          head - rest.sum
+  //TODO Finish this
+//  private def reduceNonCommutativeArithExp(exp: ArithmeticalConcolicExpression, reduce: (Int, Int) => Int): EitherReduced = {
+//    /* Ordering must be preserved. */
+//    exp.exps match {
+//      case _ :: _ :: _ =>
+//
+//        def reduceTwoArgs(a: EitherReduced, b: EitherReduced): Either[List[ArithmeticalConcolicExpression], Int] = (a, b) match {
+//          case (Right(int1), Right(int2)) => Right(reduce(int1, int2))
+//          case _ => Left(List(a, b))
+//        }
+//
+//        def reduceAllArgs(a: EitherReduced, b: EitherReduced, list: List[EitherReduced]): Either[List[ArithmeticalConcolicExpression], Int] = list match {
+//          case Nil => reduceTwoArgs(a, b)
+//          case head :: rest =>
+//        }
+//
+//        /* Need at least two arguments to reduce the expression. */
+//        val individallyReducedArgs = exp.exps.map(reduceToInt)
+//        val reducedArg1 = individallyReducedArgs.head
+//        val reducedArg2 = individallyReducedArgs(1)
+//        val reducedRest = individallyReducedArgs.tail.tail
+//
+//        val result: EitherReduced = reduceTwoArgs(reducedArg1, reducedArg2) match {
+//          case Right(int) :: Nil =>
+//            /* There were only two arguments and both were successfully reduced to integers. */
+//            Right(int)
+//          case _ =>
+//            val reducedArgs = reduceAllArgs(reducedArg1, reducedArg2, reducedRest)
+//            reducedArgs match {
+//              case Right(int) :: Nil =>
+//                /* There were more than two arguments, but all of them were successfully reduced. */
+//                Right(int)
+//              case _ =>
+//                Left(exp.copy(exps = reducedArgs))
+//            }
+//            ???
+//        }
+//        result
+//      case _ => Left(exp)
+//    }
+//  }
+
+  private def optimizeArithExp(exp: ArithmeticalConcolicExpression): EitherReduced = exp.op match {
+    case IntPlus => reduceCommutativeArithExp(exp, _.sum)
+    // TODO Allow for more than three arguments and MAKE THIS GENERIC
+    case IntMinus if exp.exps.size == 2 =>
+      val reduced1 = reduceToInt(exp.exps.head)
+      val reduced2 = reduceToInt(exp.exps(1))
+      (reduced1, reduced2) match {
+        case (Right(int1), Right(int2)) => Right(int1 - int2)
+        case (Right(int1), Left(arg2)) => Left(exp.copy(exps = List(ConcolicInt(int1), arg2)))
+        case (Left(arg1), Right(int2)) => Left(exp.copy(exps = List(arg1, ConcolicInt(int2))))
+        case (Left(arg1), Left(arg2)) => Left(exp.copy(exps = List(arg1, arg2)))
       }
-      reduceArithExp(exp, subtract)
-    case IntTimes => reduceArithExp(exp, _.product)
+    case IntMinus if exp.exps.size == 3 =>
+      val reduced1 = reduceToInt(exp.exps.head)
+      val reduced2 = reduceToInt(exp.exps(1))
+      val reduced3 = reduceToInt(exp.exps(2))
+      (reduced1, reduced2, reduced3) match {
+        case (Right(int1), Right(int2), Right(int3)) => Right(int1 - int2 - int3)
+        case (Right(int1), Right(int2), Left(arg3)) => Left(exp.copy(exps = List(ConcolicInt(int1 - int2), arg3)))
+        case (Right(int1), Left(arg2), Right(int3)) => Left(exp.copy(exps = List(ConcolicInt(int1 - int3), arg2)))
+        case (Right(int1), Left(arg2), Left(arg3)) => Left(exp.copy(exps = List(ConcolicInt(int1), arg2, arg3)))
+        case (Left(arg1), Right(int2), Right(int3)) => Left(exp.copy(exps = List(arg1, ConcolicInt(int2 - int3))))
+        case (Left(arg1), Right(int2), Left(arg3)) => Left(exp.copy(exps = List(arg1, ConcolicInt(int2), arg3)))
+        case (Left(arg1), Left(arg2), Right(int3)) => Left(exp.copy(exps = List(arg1, arg2, ConcolicInt(int3))))
+        case (Left(arg1), Left(arg2), Left(arg3)) => Left(exp.copy(exps = List(arg1, arg2, arg3)))
+      }
+    case IntMinus => throw new Exception(s"Constraint optimization does not allow optimisation of IntMinus with more than < 2 or > 3 arguments")
+    case IntTimes => reduceCommutativeArithExp(exp, _.product)
     case _ =>
       /* TODO Optimize other arithmetical operations as well */
       Left(exp)
   }
 
-  private def eitherToConcolicExp(either: Either[ConcolicExpression, Int]): ConcolicExpression = either match {
+  private def eitherToConcolicExp(either: EitherReduced): ConcolicExpression = either match {
     case Left(exp) => exp
     case Right(i) => ConcolicInt(i)
   }
@@ -65,9 +133,10 @@ object ConstraintOptimizer {
     case _ => exp
   }
 
-  private def reduceToInt(exp: ConcolicExpression): Either[ConcolicExpression, Int] = exp match {
+  private def reduceToInt(exp: ConcolicExpression): EitherReduced = exp match {
     case ConcolicInt(i) => Right(i)
     case exp: ArithmeticalConcolicExpression => optimizeArithExp(exp)
+    case _ => Left(exp)
   }
 
   def optimizeConstraint(constraint: BranchConstraint): BranchConstraint = {
