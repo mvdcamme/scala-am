5a6f81c9c14f1bf660dfd29736fc22d89085ff67
Maarten Vandercammen <mvdcamme@vub.ac.be>
2018-01-23 10:03:27 +0100

    Write logged messages also to a log-file, and not just to the console.
    The log-file is removed at the start of each Scala-AM execution

--- src/main/scala/Logger.scala
+++ src/main/scala/Logger.scala
@@ -1,7 +1,16 @@
+import java.io.FileWriter
+
 /**
   * Created by mvdcamme on 11/04/16.
   */
 object Logger {
+
+  private val logFilePath: String = "log.txt"
+
+  {
+    /* Reset the log file upon creation of the Logger object. */
+    resetLogFile()
+  }
 
   /*
    * All debug levels. Higher levels correspond to more important messages.
@@ -16,6 +25,22 @@ object Logger {
     val NoLogging = Value(Integer.MAX_VALUE)
   }
 
+  /*
+   * resetLogFile is called in the Logger-object's constructor-block. As this function uses the logFilePath-field,
+   * make sure to define this field _before_ the constructor-block, or else the field will be null.
+   */
+  private def resetLogFile(): Unit = {
+    val fileTemp = new java.io.File(logFilePath)
+    if (fileTemp.exists) {
+      fileTemp.delete()
+    }
+  }
+  private def logToFile(str: String): Unit = {
+    val writer = new FileWriter(logFilePath, true)
+    writer.write(str)
+    writer.close()
+  }
+
   val D = LogLevel.Debug
   val V = LogLevel.Verbose
   val I = LogLevel.Info
@@ -26,6 +51,7 @@ object Logger {
   private def internalLog(message: => String, messageLevel: LogLevel.Value): Unit = {
     if (messageLevel >= GlobalFlags.level) {
       println(message)
+      logToFile(message)
     }
   }
 
