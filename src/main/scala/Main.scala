7a0e527274a3fd095720e33cba9aad88dc876166
Maarten Vandercammen <mvdcamme@vub.ac.be>
2018-01-24 19:00:55 +0100

    Make sure to use BaseSchemeSemantics with Concrete symbols in the type lattice for regular analyses

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -77,7 +77,7 @@ object Main {
     Config.parser.parse(args, Config.Config()).foreach(config => {
       val lattice: SchemeLattice = config.lattice match {
           case Config.Lattice.Concrete => new MakeSchemeLattice[Concrete.S, Concrete.B, Concrete.I, Concrete.F, Concrete.C, Concrete.Sym](config.counting)
-          case Config.Lattice.TypeSet => new MakeSchemeLattice[Type.S, Concrete.B, Type.I, Type.F, Type.C, Type.Sym](config.counting)
+          case Config.Lattice.TypeSet => new MakeSchemeLattice[Type.S, Concrete.B, Type.I, Type.F, Type.C, Concrete.Sym](config.counting)
           case Config.Lattice.BoundedInt =>
             val bounded = new BoundedInteger(config.bound)
                 new MakeSchemeLattice[Type.S, Concrete.B, bounded.I, Type.F, Type.C, Type.Sym](config.counting)
@@ -103,7 +103,7 @@ object Main {
             case Config.Machine.Free => new AAMAACP4F[SchemeExp, lattice.L, address.A, time.T](P4FKAlloc)
           }
 
-          val sem = new SchemeSemantics[lattice.L, address.A, time.T](new SchemePrimitives[address.A, lattice.L])
+          val sem = new BaseSchemeSemantics[lattice.L, address.A, time.T](new SchemePrimitives[address.A, lattice.L])
 
           replOrFile(config.file, program => run(machine, sem)(program, config.dotfile, config.jsonfile, config.timeout.map(_.toNanos), config.inspect))
         case Config.Language.CScheme =>
