b840778a5950f19fb684d5c2c7b640ec032ad6a4
Maarten Vandercammen <mvdcamme@vub.ac.be>
2018-02-22 13:06:49 +0100

    Remove redundant randomConsId

--- src/main/scala/concolic/ConcolicRunTimeFlags.scala
+++ src/main/scala/concolic/ConcolicRunTimeFlags.scala
@@ -1,17 +1,10 @@
 object ConcolicRunTimeFlags {
 
-  private var randomConsId = 0
-
   val MAX_CONCOLIC_ITERATIONS = 100
 
   val checkAnalysis: Boolean = true
   val checkRunTimeAnalysis: Boolean = true
 
   def useRunTimeAnalyses: Boolean = checkAnalysis && checkRunTimeAnalysis
-
-  def newRandomConsId: Int = {
-    randomConsId += 1
-    randomConsId
-  }
 
 }
